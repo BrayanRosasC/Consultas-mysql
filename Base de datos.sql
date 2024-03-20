c31627e45072ac63be82466a52ac43a728a850d3
BrayanRosasC <162393741+BrayanRosasC@users.noreply.github.com>
2024-03-20 15:53:45 -0600

    Add files via upload

---
+++ Base de datos.sql
@@ -0,0 +1,3 @@
+Create TABLE talk.progreso (
+    id_progreso INT auto_increment not null, descripccion_progreso varchar(55) not null, progreso varchar(100), Primary Key (id_progreso)
+) Engine = INNODB;
