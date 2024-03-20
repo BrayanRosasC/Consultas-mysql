e3f016680b2491b28c2479bf97fe7facf78e4cb8
BrayanRosasC <162393741+BrayanRosasC@users.noreply.github.com>
2024-03-20 15:55:47 -0600

    Update Base de datos.sql

--- Base de datos.sql
+++ Base de datos.sql
@@ -1,3 +1,9 @@
+CREATE DATABASE talk;
+Create table talk.Usuario(id_usuario INT auto_increment not null, Nombre varchar(55) not
+null, apellido_P varchar(55) not null, apellido_M varchar(55) not null, fech_nac Date not
+null, teléfono varchar(10) not null, email varchar(255) not null, fecha_reg date not null, username varchar(55) not null,
+pasword varchar(255) not null, Primary Key(id_usuario))
+Engine=INNODB;
 Create TABLE talk.progreso (
     id_progreso INT auto_increment not null, descripccion_progreso varchar(55) not null, progreso varchar(100), Primary Key (id_progreso)
 ) Engine = INNODB;
